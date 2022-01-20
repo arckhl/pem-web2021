b8d73412374766f674c00f380a9c6a5d2041a37e
arckhl <91888029+arckhl@users.noreply.github.com>
2022-01-20 20:59:29 +0700

    Add files via upload

---
+++ Tugas Akhir Smt 1/Tampilan.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Tugas Akhir</title>
+        <link rel="stylesheet" href="style web utama.css">
+        <link rel="stylesheet" href="fontawesome/css/all.css">
+    </head>
+    <body>
+        <div class="judul">SEARCH JOB</div>
+        <div class="search-box">
+            <input class="search-txt" type="text" placeholder="Type to search">
+            <div class="search-btn">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+        <div class="notif-box">
+            <p class="notif-txt"></p>
+            <div class="notif-btn">
+                <a href="notifikasi.html"<button><i class="far fa-bell"></i></button>
+            </div>
+        </div>
+        <div class="akun" >
+            <div class="akun-btn">
+                <a href="form login.html"<button><i class="far fa-user"></i></button>
+            </div>
+        </div>
+        <div class="perusahaan">
+            <div class="perusahaan-btn">
+                <a href="building.html"<button><i class="fas fa-building"></i></button>
+            </div>
+        </div>
+        <div class="maps">
+            <div class="maps-btn">
+                <a href="https://www.google.com/maps"<button><i class="fas fa-map-marker-alt"></i></button>
+            </div>
+        </div>
+        <div class="job">
+            <div class="job-btn">
+                <a href="job.html"<button><i class="fas fa-search-dollar"></i></button>
+            </div>
+        </div>
+    </body>
+</html>
